361bdf9fbc7fb59062c8d890d6ca8578c8e6b2f7
rapa5960 <rapa5960@colorado.edu>
2018-04-29 20:09:01 -0600

    Update textpoof.js

--- textpoof.js
+++ textpoof.js
@@ -1,16 +1,5 @@
 
-$("#breath_button").click(function() {
-	setTimeout(inFunc, 3000);
-});
-
-function inFunc() {
-	document.getElementById("prompt_text").innerHTML = "Breathe In";
-	document.getElementById("prompt_text").style.backgroundColor = '#46c0c9';
-	setTimeout(outFunc, 5000);
-}
-
-function outFunc() {
-	document.getElementById("prompt_text").innerHTML = "Breathe Out";
-	document.getElementById("prompt_text").style.backgroundColor = '#00ff26';
-	setTimeout(inFunc, 7000);
-}
+$("#clear").click(function() {
+	$("#write_zone").val("");
+    document.getElementById("poof").style.visibility = "visible";
+})
